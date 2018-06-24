81b816ff3a79dabc10cf132ace2d93209e4c8e50
ravindra1001 <ravindra.saini1001@gmail.com>
2018-06-24 23:42:34 +0530

    testing

--- geeksforgeeks/bst/test1.cpp
+++ geeksforgeeks/bst/test1.cpp
@@ -1,25 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
-struct TreeNode{
-	int val;
-	TreeNode * left;
-	TreeNode * right;
-	TreeNode(int temp){
-		this->val = temp;
-		this->left = NULL;
-		this->right = NULL;		
-	}
-};
 
-bool fun(TreeNode * root){
-	if(root==NULL) return true;
-	if(root->left!= NULL && root->right != NULL) return false;
-	else return fun(root->left) && fun(root->right);	
+void fun(){
+	cout<<"hello world"<<endl;
 }
 int main(){
-	TreeNode * root = new TreeNode(5);
-	root->left = new TreeNode(3);
-	// root->right = new  TreeNode(8);
-	cout<<fun(root)<<endl;
+	fun();
 	return 0;
 }
